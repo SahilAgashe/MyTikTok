1034c5a08698ca861964ca9d3990d904fc174133
Sahil Agashe <sahil@scrambleapps.gmail.com>
2023-12-29 22:45:13 +0530

    added MainTabView and setting up tabbar

--- My_TikTok/App/My_TikTokApp.swift
+++ My_TikTok/App/My_TikTokApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct My_TikTokApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainTabView()
         }
     }
 }
---
+++ My_TikTok/Core/TabBar/MainTabView.swift
@@ -0,0 +1,72 @@
+//
+//  MainTabView.swift
+//  My_TikTok
+//
+//  Created by SAHIL AMRUT AGASHE on 29/12/23.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    // MARK: - Properties
+    @State private var selectedTab = 0
+    
+    // MARK: - body
+    var body: some View {
+        TabView() {
+            // Home
+            Text("Feed")
+                .tabItem {
+                    VStack {
+                        Image(systemName: "house")
+                            .environment(\.symbolVariants, selectedTab == 0 ? .fill : .none)
+                        Text("Home")
+                    }
+                }
+                .onAppear{selectedTab = 0}
+                .tag(0)
+            
+            Text("Friends")
+                .tabItem {
+                    VStack {
+                        Image(systemName: "person.2")
+                            .environment(\.symbolVariants, selectedTab == 1 ? .fill : .none)
+                            Text("Home")
+                    }
+                }
+                .onAppear{selectedTab = 1}
+            
+            Text("Upload Post")
+                .tabItem {
+                    Image(systemName: "plus")
+                        .environment(\.symbolVariants, selectedTab == 2 ? .fill : .none)
+                }
+                .onAppear{selectedTab = 2}
+                
+            Text("Notifications")
+                .tabItem {
+                    VStack {
+                        Image(systemName: "heart")
+                            .environment(\.symbolVariants, selectedTab == 3 ? .fill : .none)
+                        Text("Home")
+                    }
+                }
+                .onAppear{selectedTab = 3}
+                
+            Text("Profile")
+                .tabItem {
+                    VStack {
+                        Image(systemName: "person")
+                            .environment(\.symbolVariants, selectedTab == 4 ? .fill : .none)
+                        Text("Home")
+                    }
+                }
+                .onAppear{selectedTab = 4}
+        }
+        .tint(.black)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
