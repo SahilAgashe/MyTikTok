1c243660b6a00be0a071d7f1fb2c7b47dde9ca8f
Sahil Agashe <sahil@scrambleapps.gmail.com>
2023-12-29 21:28:56 +0530

    Initial Commit

---
+++ My_TikTok/My_TikTokApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_TikTokApp.swift
+//  My_TikTok
+//
+//  Created by SAHIL AMRUT AGASHE on 29/12/23.
+//
+
+import SwiftUI
+
+@main
+struct My_TikTokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
